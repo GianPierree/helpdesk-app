758fbfff4fc0a630af6dfc3fcf0d516a571f2cf0
Gian Pierree Muñoz <gian0291@gmail.com>
2022-12-26 14:34:29 -0500

    app Bitrix24

--- index.php
+++ index.php
@@ -16,18 +16,21 @@
 		?>
 	</div>
 	<script>
-		BX24.callMethod(
-			"crm.lead.get", 
-			{ id: 8 }, 
-			function(result) {
-				if(result.error()){
-					console.error(result.error());
+		BX24.init(function(){
+			// console.log('Init done!', BX24.isAdmin());
+			BX24.callMethod(
+				"crm.lead.get", 
+				{ id: 8 }, 
+				function(result) {
+					if(result.error()){
+						console.error(result.error());
+					}
+					else{
+						console.dir(result.data());
+					}
 				}
-				else{
-					console.dir(result.data());
-				}
-			}
-		);	
+			);	
+		});
 	</script>
 </body>
 </html>
