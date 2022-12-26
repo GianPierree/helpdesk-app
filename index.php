64e7fa3e673cba2a9f21839865e04ccbf9c4a275
Gian Pierree Muñoz <gian0291@gmail.com>
2022-12-26 15:23:35 -0500

    Bootstrap

--- index.php
+++ index.php
@@ -14,21 +14,10 @@
 <body>
 	<div id="name">
 		<div class="row g-2">
-			<div class="col-md">
+			<div class="col">
 				<div class="form-floating">
-					<input type="email" class="form-control" id="floatingInputGrid" placeholder="name@example.com" value="mdo@example.com">
-					<label for="floatingInputGrid">Email address</label>
-				</div>
-			</div>
-			<div class="col-md">
-				<div class="form-floating">
-					<select class="form-select" id="floatingSelectGrid">
-						<option selected>Open this select menu</option>
-						<option value="1">One</option>
-						<option value="2">Two</option>
-						<option value="3">Three</option>
-					</select>
-					<label for="floatingSelectGrid">Works with selects</label>
+					<input type="text" class="form-control" id="floatingInputGrid" placeholder="" value="">
+					<label for="floatingInputGrid">Buscar ticket</label>
 				</div>
 			</div>
 		</div>
