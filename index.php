bfe9ebbc14d6a6adaa564b5768bac88d1684ca1f
Gian Pierree Muñoz <gian0291@gmail.com>
2022-12-26 15:19:14 -0500

    Bootstrap

--- index.php
+++ index.php
@@ -1,20 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>App 2612</title>
+	
+	<!-- Bitrix24 -->
 	<script src="//api.bitrix24.com/api/v1/dev/"></script>
+	
+	<!-- Bootstrap -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
 	<div id="name">
-		<?php
-		require_once (__DIR__.'/crestcurrent.php');
-
-		//$result = CRest::call('user.current');
-		$result = CRestCurrent::call('user.current');
-
-		echo $result['result']['NAME'].' '.$result['result']['LAST_NAME'];
-		?>
+		<div class="row g-2">
+			<div class="col-md">
+				<div class="form-floating">
+					<input type="email" class="form-control" id="floatingInputGrid" placeholder="name@example.com" value="mdo@example.com">
+					<label for="floatingInputGrid">Email address</label>
+				</div>
+			</div>
+			<div class="col-md">
+				<div class="form-floating">
+					<select class="form-select" id="floatingSelectGrid">
+						<option selected>Open this select menu</option>
+						<option value="1">One</option>
+						<option value="2">Two</option>
+						<option value="3">Three</option>
+					</select>
+					<label for="floatingSelectGrid">Works with selects</label>
+				</div>
+			</div>
+		</div>
 	</div>
 	<script>
 		BX24.init(function(){
@@ -33,5 +50,8 @@
 			);	
 		});
 	</script>
+
+	<!-- Bootstrap -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
